c269268d74bcd718fb82a0c56d58a3fc05e421e4
Swaroop Bhanja <79194630+swaroopb2977@users.noreply.github.com>
2023-11-30 12:12:07 +0530

    Created django Forms directory

---
+++ basicforms/basicapp/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.core import validators
+
+'''
+user defined validation
+
+def check_for_z(value):
+ if value[0].lower() == 'z':
+  raise forms.ValidationError("Name must start from z")
+  
+in class FormName:
+name=forms.CharField(validators=[check_for_z])
+
+
+'''
+
+class FormName(forms.Form):
+    name=forms.CharField()
+    email=forms.EmailField()
+    verify_email=forms.EmailField(label='Enter your email again:')
+    text=forms.CharField(widget=forms.Textarea)
+    botcatcher=forms.CharField(required=False,
+                               widget=forms.HiddenInput,
+                               validators=[validators.MaxLengthValidator(0)])
+    
+    def clean(self): #check to whether mailid matches or not
+        all_clean_data=super().clean()
+        email=all_clean_data['email']
+        vmail =all_clean_data['verify_email']
+        
+        if email!= vmail:
+            raise forms.ValidationError('Make sure email match ')
+        
+''' 
+EXTERNAL VALIDATOR FOR CATCHING BOT 
+def clean_botcatcher(self):
+        botcatcher=self.cleaned_data["botcatcher"]
+        if len(botcatcher)>0:
+            raise forms.ValidationError("Gotcha bot")
+        return botcatcher
+        '''
